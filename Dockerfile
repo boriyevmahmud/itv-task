748dd727885d9c37053a45cf3354f81ee4d8e92e
Makhmud <mahmud4563253@gmail.com>
2025-03-24 11:30:48 +0500

    added docker

---
+++ Dockerfile
@@ -0,0 +1,25 @@
+FROM golang:1.22.4 AS builder
+
+WORKDIR /app
+
+COPY go.mod ./
+COPY go.sum ./
+
+RUN go mod download
+
+COPY . .
+
+WORKDIR /app/cmd
+
+RUN CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o /app/myapp .
+
+FROM alpine:latest
+RUN apk --no-cache add ca-certificates
+
+WORKDIR /root/
+
+COPY --from=builder /app/myapp .
+
+EXPOSE 8080
+
+CMD ["./myapp"]
--- config/config.go
+++ config/config.go
@@ -25,14 +25,15 @@ func Load() Config {
 	// Load environment variables from .env file
 	if err := godotenv.Load(".env"); err != nil {
 		fmt.Println("Error loading .env file:", err)
+		// panic(err)
 	}
 
 	return Config{
-		PostgresHost:     cast.ToString(getOrDefault("POSTGRES_HOST", "localhost")),
+		PostgresHost:     cast.ToString(getOrDefault("POSTGRES_HOST", "localhost1")),
 		PostgresPort:     cast.ToInt(getOrDefault("POSTGRES_PORT", 5432)),
-		PostgresUser:     cast.ToString(getOrDefault("POSTGRES_USER", "postgres")),
-		PostgresPassword: cast.ToString(getOrDefault("POSTGRES_PASSWORD", "password")),
-		PostgresDatabase: cast.ToString(getOrDefault("POSTGRES_DATABASE", "movies_db")),
+		PostgresUser:     cast.ToString(getOrDefault("POSTGRES_USER", "postgres1")),
+		PostgresPassword: cast.ToString(getOrDefault("POSTGRES_PASSWORD", "password1")),
+		PostgresDatabase: cast.ToString(getOrDefault("POSTGRES_DATABASE", "movies_db1")),
 		JWTAccessSecret:  cast.ToString(getOrDefault("JWT_ACCESS_SECRET", "access_secret")),
 		JWTRefreshSecret: cast.ToString(getOrDefault("JWT_REFRESH_SECRET", "refresh_secret")),
 
